bc4ccc2564ff38fa1dbdfbebda522f575de51e9f
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-21 12:32:29 +0300

    Код для задачи4

---
+++ Task104/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numberEven = 2;
+
+if(numberEven > 1)
+{
+    while(number >= numberEven)
+    {
+        Console.Write(numberEven + " ");
+        numberEven = numberEven + 2;
+    }
+}
