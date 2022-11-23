41871949226e18c63202e98d11547b4a52e02beb
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-23 23:06:07 +0300

    Написали код к задаче203

--- Task203/Program.cs
+++ Task203/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+  if (number == 6 || number == 7) 
+  /*Условный логический оператор || OR также вычисляет логический ИЛИ операндов, 
+  но не вычисляет правый операнд, если левый операнд имеет true значение .*/
+  {
+    Console.WriteLine(" Выходной ");
+  }
+  
+  else 
+  Console.WriteLine(" Рабочий ");
+
+
