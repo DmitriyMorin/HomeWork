cebf612ec76bcec711c3c4c4ae1897aa81fd9c2b
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-23 20:04:24 +0300

    Наисали код к задаче201

---
+++ Task201/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+int B = (number / 10) % 10;
+
+Console.WriteLine(B);
