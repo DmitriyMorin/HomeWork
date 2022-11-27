64d7b9ff23271fd84367d66b861d4122a86e0b01
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-27 16:01:47 +0300

    Код к задаче Task402

---
+++ Task402/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumNumber(int number)
+{
+ int count = Convert.ToString(number).Length;
+ int advance = 0;
+ int result = 0;
+ int i = 0;
+
+    while (i < count)
+    {
+      advance = number - number % 10;
+      result = result + (number - advance);
+      number = number / 10;
+      i++;
+    }
+   return result;
+}
+
+Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int sumNumber = SumNumber(numberN);
+Console.WriteLine(sumNumber);
