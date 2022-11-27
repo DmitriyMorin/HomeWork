f51fe791cfe5d4918c1c1629ed1f401955496ee7
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-27 15:06:52 +0300

    Код к задаче Task401

---
+++ Task401/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите метод, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B. 
+Math.Pow использовать нельзя*/
+
+int NaturalDegree(int numberA, int numberB)
+{
+  int result = 1;
+  int i = 1;
+while (i <= numberB)
+ {
+    result = result * numberA;
+    i++;
+ }
+ return result;
+}
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int exponentiation = NaturalDegree(numberA, numberB);
+Console.WriteLine(exponentiation);
