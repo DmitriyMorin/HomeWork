1c99767b3f79f0325740db1dcadb68e83b39bdec
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-27 22:18:10 +0300

    Task403

---
+++ Task403/Program.cs
@@ -0,0 +1,9 @@
+﻿using static Library;
+// Напишите методы, который задаёт массив из 8 элементов и выводит их на экран.
+
+
+int countElements = GetNumber("Введите количество эл массива: ");
+int[] arr = CreateArray(countElements);
+Fill(arr);
+PrintGood(arr);
+Console.WriteLine(PrintGood(arr));
