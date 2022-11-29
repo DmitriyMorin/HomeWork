285c95c858fe1d8f50be93c1fbb665e5c0fbb899
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-29 21:20:21 +0300

    Клиенский код Task503

---
+++ Task503/Program.cs
@@ -0,0 +1,12 @@
+﻿using static Library;
+/*Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.*/
+
+Console.WriteLine("Введите количество эл массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumber(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintBad(numbers);
+double result = (numbers);
+Console.WriteLine($"{result} ");
