866f3c6103dcf703bf404b316cac73caf603653a
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-29 20:34:06 +0300

    Написали начало клиенского кода Task502

---
+++ Task502/Program.cs
@@ -0,0 +1,12 @@
+﻿using static Library;
+/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.*/
+
+Console.WriteLine("Введите количество эл массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumber(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintBad(numbers);
+int result = (numbers);
+Console.WriteLine($"{result} сумма");
