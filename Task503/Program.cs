b53746081b2219849f89b30252de1f23967f8991
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-29 23:11:53 +0300

    код Task503

--- Task503/Program.cs
+++ Task503/Program.cs
@@ -8,5 +8,18 @@ double[] numbers = new double[size];
 FillArrayRandomNumber(numbers);
 Console.WriteLine("Вот наш массив: ");
 PrintBad(numbers);
-double result = (numbers);
-Console.WriteLine($"{result} ");
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+}
+Console.WriteLine($"Разность = {max - min}");
