7634b08c211d6f67f212dbeec6e93f3de044b75d
Dmitrii Morin <dimka.morin@gmail.com>
2022-11-29 20:07:09 +0300

    Написали код к Task501

---
+++ Task501/Program.cs
@@ -0,0 +1,13 @@
+﻿using static Library;
+/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве*/
+
+Console.WriteLine("Введите количество эл массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumber(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintBad(numbers);
+int result = CountIsEven(numbers);
+Console.WriteLine($"{result} четных");
+
