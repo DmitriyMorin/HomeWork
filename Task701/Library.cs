d2c6222786a8a9d85c9b027da7fa47b395a4d6d0
Dmitrii Morin <dimka.morin@gmail.com>
2022-12-02 20:58:33 +0300

    Добавили библиотеку

---
+++ Task701/Library.cs
@@ -0,0 +1,56 @@
+public static class Library
+{
+  public static int[,] CreateTriangle(int row)
+{
+  int[,] triangle = new int[row, row];
+  for (int i = 0; i < row; i++)
+  {
+    triangle[i, 0] = 1;
+    triangle[i, i] = 1;
+  }
+
+  for (int i = 2; i < row; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      triangle[i, j] =
+      triangle[i - 1, j - 1] + triangle[i - 1, j];
+    }
+  }
+  return triangle;
+}
+public static void PrintTriangle(int[,] triangle)
+{
+  int row = triangle.GetLength(0);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < row; j++)
+    {
+      if (triangle[i, j] % 2!= 0)
+        Console.Write("*");
+        else
+        {
+            Console.Write(" ");
+        }
+
+    }
+    Console.WriteLine();
+  }
+
+
+
+
+}
+
+public static int[] GetKoeff(int[,] tr, int pow)
+{
+  int[] row = new int[pow + 1];
+  for (int i = 0; i <= pow; i++)
+  {
+    row[i] = tr[pow, i];
+  }
+  return row;
+}
+
+
+}
