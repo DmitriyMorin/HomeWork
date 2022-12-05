baddd1ae0b6c6dbcc87c8b48b707a1d87101c5f3
Dmitrii Morin <dimka.morin@gmail.com>
2022-12-05 09:02:43 +0300

    код к Task902

---
+++ Task902/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.*/
+
+int n, m, s=0;
+Console.Write("Введи значение от: ");
+n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи значение до: ");
+m = Convert.ToInt32(Console.ReadLine());
+for (int i = n; i <= m; i++)
+    s += i;
+Console.WriteLine(s);
